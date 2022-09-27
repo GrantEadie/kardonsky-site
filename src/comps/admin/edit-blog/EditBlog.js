cbb29e04a8548b0c69596a3350aa75b195ade68b
Grant Eadie <granteadie@Grants-iMac-2.local>
2022-09-26 20:34:06 -0700

    finalize the useeffect in the edit functions.

--- src/comps/admin/edit-blog/EditBlog.js
+++ src/comps/admin/edit-blog/EditBlog.js
@@ -1,24 +1,32 @@
 import "./new-blog.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ArrowRight } from "phosphor-react";
 import { updateDocument } from "../../../hooks/useFirestore";
 import UploadForm from "../upload/UploadMultiple";
 import UploadImageGrid from "../upload/UploadImageGrid";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import useGetDocs from "../../../hooks/useFirestore";
+import { getStuff } from "../../../hooks/useFirestore";
 
 const inputs = ["title", "date"];
 
 const EditBlog = () => {
   const { blogId } = useParams();
   const [newDocument, setNewDocument] = useState({});
-  const { docs } = useGetDocs("blog");
+  const [docs, setDocs] = useState([]);
   const [docId] = useState(blogId);
   const blog = docs.find((data) => data.id === blogId);
   const [urls, setURLs] = useState([]);
   const [currentlyUploading, setCurrentlyUploading] = useState(1);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const gotDocs = await getStuff("blog");
+      setDocs(gotDocs.sort((a, b) => a.createdAt - b.createdAt));
+    };
+    fetchData();
+  }, [setDocs]);
 
   const handleNewDocumentChange = (e) => {
     const key = e.target.name;
--- src/comps/admin/edit-listing/EditListing.js
+++ src/comps/admin/edit-listing/EditListing.js
@@ -1,12 +1,11 @@
 import "./edit-listing.css";
 import { useEffect, useState } from "react";
 import { ArrowRight } from "phosphor-react";
-import { updateDocument } from "../../../hooks/useFirestore";
+import { getStuff, updateDocument } from "../../../hooks/useFirestore";
 import UploadForm from "../upload/UploadMultiple";
 import UploadImageGrid from "../upload/UploadImageGrid";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import useGetDocs from "../../../hooks/useFirestore";
 
 const inputs = [
   "address",
@@ -20,13 +19,21 @@ const inputs = [
 
 const EditListing = () => {
   const { listingId } = useParams();
-  const { docs } = useGetDocs("listings");
+  const [docs, setDocs] = useState([]);
   const listing = docs.find((data) => data.id === listingId);
   const [newDocument, setNewDocument] = useState({});
   const [docId] = useState(listingId);
   const [urls, setURLs] = useState([]);
   const [currentlyUploading, setCurrentlyUploading] = useState(1);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const gotDocs = await getStuff("listings");
+      setDocs(gotDocs.sort((a, b) => a.createdAt - b.createdAt));
+    };
+    fetchData();
+  }, [setDocs]);
 
   useEffect(() => {
     if (listing) {
